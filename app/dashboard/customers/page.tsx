02d2bf614170d68fa3ee25cc5fc73aea5699cc47
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-09-20 14:25:36 -0700

    added changes up to chapter 5 of the next.js tutorial

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Customers() {
+  return <p>Customer Pages</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Invoices() {
+  return <p>Invoices Page</p>;
+}
